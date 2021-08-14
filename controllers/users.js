f85f3c4ac69d512d94b14404466a47d220163c51
SarthakGirotra <sarthakgirotra@gmail.com>
2021-08-14 20:16:49 +0530

    v1

---
+++ controllers/users.js
@@ -0,0 +1,68 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import User from '../models/user.js'
+import dotenv from 'dotenv'
+dotenv.config();
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) return res.status(404).json({ message: "User doesnt exist" })
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
+        if (!isPasswordCorrect) return res.status(400).json({ message: "invalid credentials" })
+
+        const token = jwt.sign({ email: existingUser, id: existingUser._id }, process.env.SECRET_KEY, { expiresIn: "1h" })
+        res.status(200).json({ result: existingUser, token })
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+
+}
+
+export const home = (req, res) => {
+
+    const token = req.headers?.jwt;
+    try {
+        if (!token)
+            res.status(401).json({ message: "No token in request" })
+        else {
+
+            jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+                if (err)
+                    return res.status(401).send({ auth: false, message: err });
+                // console.log(decoded)
+                const existingUser = User.findOne({ email: decoded?.email });
+                if (!existingUser) return res.status(404).json({ message: "User doesnt exist" })
+
+                return res.status(200).send("authenticated")
+            });
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+
+}
+
+export const signup = async (req, res) => {
+
+    const { name, email, password } = req.body;
+    try {
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) return res.status(400).json({ message: "User already exists" })
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const result = await User.create({ email, password: hashedPassword, name })
+        const token = jwt.sign({ email: result.email, id: result._id }, process.env.SECRET_KEY, { expiresIn: "1h" })
+        res.status(200).json({ result, token })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+
+}
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { login, signup, home } from '../controllers/users.js'
+
+const router = express.Router();
+
+router.post('/login', login)
+router.post('/signup', signup)
+router.post('/home', home)
+
+export default router;
